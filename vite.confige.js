2db233173e9b6082f4c8765670df734a68384630
AlekseiBaranov <baranov.sys@gmail.com>
2024-06-20 20:19:15 +0300

    pr

--- vite.confige.js
+++ vite.confige.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import path from 'path'
+import { defineConfig } from 'vite';
 
 export default defineConfig ({
     plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: path.resolve(__dirname, './public/vite.svg' ),
-                    dest: './'
-                }
-            ]
-        })
+        
     ]
 })
